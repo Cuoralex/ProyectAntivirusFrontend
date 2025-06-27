aa86cfad47a62635447f5cfac07de9367025af97
Cuoralex <alexacuartas@gmail.com>
2025-06-27 18:12:33 -0500

    Despliegue en netlify

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  future: {},
+  future: {
+    v2_meta: true,
+    v2_routeConvention: true,
+  },
+  serverBuildTarget: "netlify",
+  server: "./server.ts",
   ignoredRouteFiles: ["**/.*"],
-  serverBuildDirectory: "build/server",
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
+  serverBuildPath: "netlify/functions/server.js",
+  devServerPort: 8002,
 };
-
