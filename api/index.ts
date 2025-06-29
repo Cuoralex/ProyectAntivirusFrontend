d0ed4f6f061dea8f6c10159523dd2f164ca1d0ee
Cuoralex <alexacuartas@gmail.com>
2025-06-29 15:57:37 -0500

    Actualización codigo para vercel

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+// api/index.ts
+import { createRequestHandler } from "@vercel/remix";
+import * as build from "../build/index.js";
+
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
